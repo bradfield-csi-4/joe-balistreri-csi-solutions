bdade5f13f4c9c35e15763301d4b8c4451dfcc4b
Joe Balistreri <jbalistreri@twitter.com>
2021-08-09 08:40:56 -0700

    flag setting is done

--- prep/c/ls-clone/ls.c
+++ prep/c/ls-clone/ls.c
@@ -18,25 +18,15 @@ void my_ls(char *);
 
 int main(int argc, char **argv)
 {
-  // handle the case with no flags or files specified
-  if (argc == 1) {
-    my_ls(".");
-    return 0;
-  }
-
   // find all the flags
   while (--argc > 0)
     if ((*++argv)[0] == '-') {
       while (isalpha((++(*argv))[0])) {
         switch((*argv)[0]) {
           case 'a':
-            if (show_hidden_files == 0)
-              printf("showing hidden files\n");
             show_hidden_files = 1;
             break;
           case 'l':
-            if (long_format == 0)
-              printf("using long format\n");
             long_format = 1;
             break;
           default:
@@ -51,14 +41,16 @@ int main(int argc, char **argv)
       break;
     }
 
-  if (argc > 1) {
-    printf("has multiple files!!\n");
-    has_multiple_files = 1;
+  // print information for each directory
+  if (argc == 0) {
+    my_ls(".");
+  } else {
+    if (argc > 2) {
+      has_multiple_files = 1;
+    }
+    while (--argc > 0)
+      my_ls(*++argv);
   }
-
-  while (--argc > 0)
-    my_ls(*++argv);
-
   return 0;
 }
 
@@ -86,7 +78,7 @@ void my_ls(char *name)
       if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
         continue;
       if (strlen(dir)+strlen(dp->d_name)+2 > sizeof(name))
-        fprintf(stderr, "dirwalk: name %s/%s too long \n", dir, dp->d_name);
+        fprintf(stderr, "my_ls: name %s/%s too long \n", dir, dp->d_name);
       else {
         fprintf(stdout, "%s\t", dp->d_name);
         sprintf(name, "%s/%s", dir, dp->d_name);
