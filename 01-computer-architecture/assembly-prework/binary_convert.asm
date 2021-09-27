fd59b57e5fbf219de49b9c2d015c1dfe9d03db73
Joe Balistreri <jbalistreri@twitter.com>
2021-09-27 10:10:36 -0700

    binaryconvert works

--- 01-computer-architecture/assembly-prework/binary_convert.asm
+++ 01-computer-architecture/assembly-prework/binary_convert.asm
@@ -1,4 +1,18 @@
 section .text
 global binary_convert
 binary_convert:
+.start:
+.top:
+	mov bl, [rdi]
+	cmp bl, byte 0 				; compare to null terminator
+	je .exit								; if <, it's the null terminator
+
+	inc rdi 								; move to the next position
+	sal rax, 1							; multiply rax by 2
+	cmp bl, byte 48  			; compare to zero ascii character
+	je .top
+
+	inc rax
+	jmp .top
+.exit:
 	ret
