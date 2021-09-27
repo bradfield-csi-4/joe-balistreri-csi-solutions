0d14f487bbe4874792440851b848de3bf599f850
Joe Balistreri <jbalistreri@twitter.com>
2021-09-27 10:17:02 -0700

    some optimization on binary convert

--- 01-computer-architecture/assembly-prework/binary_convert.asm
+++ 01-computer-architecture/assembly-prework/binary_convert.asm
@@ -2,17 +2,13 @@ section .text
 global binary_convert
 binary_convert:
 .start:
-.top:
 	mov bl, [rdi]
-	cmp bl, byte 0 				; compare to null terminator
-	je .exit								; if <, it's the null terminator
+	sub bl, 48			 				; subtract the value of the 0 ascii character
+	js .exit								; if negative, bl was the null terminator
 
 	inc rdi 								; move to the next position
 	sal rax, 1							; multiply rax by 2
-	cmp bl, byte 48  			; compare to zero ascii character
-	je .top
-
-	inc rax
-	jmp .top
+	add al, bl
+	jmp .start
 .exit:
 	ret
