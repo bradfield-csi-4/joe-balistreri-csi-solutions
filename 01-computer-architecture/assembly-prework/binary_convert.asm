d0b288906e711a4588435d6fe85b39c44b2294a8
Joe Balistreri <jbalistreri@twitter.com>
2021-09-27 11:52:19 -0700

    pangrams is working

--- 01-computer-architecture/assembly-prework/binary_convert.asm
+++ 01-computer-architecture/assembly-prework/binary_convert.asm
@@ -1,13 +1,12 @@
 section .text
 global binary_convert
 binary_convert:
-.start:
 	mov bl, [rdi]
 	sub bl, 48			 				; subtract the value of the 0 ascii character
 	js .exit								; if negative, bl was the null terminator
 
 	inc rdi 								; move to the next position
-	lea rax, [rbx, rax*2]
-	jmp .start
+	lea rax, [rbx, rax*2]   ; double rax and add the value in rbx
+	jmp binary_convert
 .exit:
 	ret
--- 01-computer-architecture/assembly-prework/pangram.asm
+++ 01-computer-architecture/assembly-prework/pangram.asm
@@ -1,4 +1,41 @@
 section .text
 global pangram
 pangram:
+	dec rdi
+.top:
+	inc rdi
+	mov cl, [rdi]
+
+	; exit if we've hit the end of the string
+	cmp cl, 0
+	je .exit
+
+	; go back to the top if the value is <65
+	sub cl, 65
+	js .top
+
+	; store the value if it's in the right range (A-Z)
+	cmp cl, 26
+	jl .store
+
+	; subtract 32 to account for a-z
+	sub cl, 32
+	; if it goes negative, skip because we're in 91-96
+	js .top
+	; if >= 26, we're above a-z
+	cmp cl, 26
+	jge .top
+
+.store:
+	mov rbx, 1
+	sal rbx, cl
+	or rax, rbx
+	jg .top
+.exit:
+	sub rax, 0b11111_11111_11111_11111_111111
+	js .real_exit
+	mov rax, 1
+	ret
+.real_exit:
+	mov rax, 0
 	ret
