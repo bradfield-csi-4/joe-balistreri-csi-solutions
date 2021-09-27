e08e67f6beb68d66a554329b70ed110018793946
Joe Balistreri <jbalistreri@twitter.com>
2021-09-27 12:43:59 -0700

    more optimizations

--- 01-computer-architecture/assembly-prework/pangram.asm
+++ 01-computer-architecture/assembly-prework/pangram.asm
@@ -1,10 +1,8 @@
 section .text
 global pangram
 pangram:
-	dec rdi
-.top:
-	inc rdi
 	mov cl, [rdi]
+	inc rdi
 
 	; exit if we've hit the end of the string
 	cmp cl, 0
@@ -12,19 +10,18 @@ pangram:
 
 	; go back to the top if the value is <65
 	sub cl, 65
-	js .top
+	js pangram
 
 .store:
 	mov rbx, 1
 	sal rbx, cl
 	or rax, rbx
-	jg .top
+	jg pangram
 .exit:
 	mov rbx, rax
 	shr rbx, 32 					; shift a-z into A-Z positions
 	or rax, rbx
-	sal rax, 38						; clear the 38 high bits of rax
-	shr rax, 38
+	and rax, 0b11111_11111_11111_11111_111111 ; clear the 38 high bits of rax
 	sub rax, 0b11111_11111_11111_11111_111111
 	js .real_exit
 	mov rax, 1
