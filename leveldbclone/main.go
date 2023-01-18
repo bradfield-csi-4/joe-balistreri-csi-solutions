b2536219c8fc50684d8a432f6db5dc03ef6fe6b4
Joe Balistreri <jdbalistreri@gmail.com>
2023-01-17 21:55:05 -0500

    more wal stuff

--- leveldbclone/main.go
+++ leveldbclone/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	"fmt"
 
 	"github.com/jdbalistreri/bradfield-csi-solutions/leveldbclone/db"
 )
@@ -17,28 +16,33 @@ func main() {
 	// }
 	// fmt.Println(string(v))
 
-	rand.Seed(time.Now().UnixNano())
+	// rand.Seed(time.Now().UnixNano())
 
-	sl := db.NewSkipList(db.MAX_LEVEL)
-	// sl.PredeterminedLevels = []int{0, 1, 0, 0, 2, 1, 2, 3}
-	sl.Put([]byte("hello"), []byte("goodbye"))
-	sl.Put([]byte("apple"), []byte("pie"))
-	sl.Put([]byte("aaa"), []byte("cake"))
-	sl.Put([]byte("bvb"), []byte("cake"))
-	sl.Put([]byte("ababaaa"), []byte("cake"))
-	sl.Put([]byte("zebra"), []byte("cake"))
-	sl.Put([]byte("chewie"), []byte("boy"))
-	sl.Put([]byte("apple"), []byte("cry"))
-	sl.Put([]byte("apple"), []byte("cry"))
-	sl.Put([]byte("jiuce"), []byte("juice"))
-	sl.Put([]byte("juicy"), []byte("juice"))
-	sl.Put([]byte("change"), []byte("juice"))
-	sl.Put([]byte("bang"), []byte("juice"))
-	sl.Put([]byte("alpha"), []byte("juice"))
-	sl.Put([]byte("jiuce"), []byte("juice"))
-	sl.Put([]byte("crazy"), []byte("juice"))
-	sl.Put([]byte("casdfasdfrazy"), []byte("juice"))
-	sl.Put([]byte("asdf"), []byte("juice"))
+	// sl := db.NewSkipList(db.MAX_LEVEL)
+	// // sl.PredeterminedLevels = []int{0, 1, 0, 0, 2, 1, 2, 3}
+	// sl.Put([]byte("hello"), []byte("goodbye"))
+	// sl.Put([]byte("apple"), []byte("pie"))
+	// sl.Put([]byte("aaa"), []byte("cake"))
+	// sl.Put([]byte("bvb"), []byte("cake"))
+	// sl.Put([]byte("ababaaa"), []byte("cake"))
+	// sl.Put([]byte("zebra"), []byte("cake"))
+	// sl.Put([]byte("chewie"), []byte("boy"))
+	// sl.Put([]byte("apple"), []byte("cry"))
+	// sl.Put([]byte("apple"), []byte("cry"))
+	// sl.Put([]byte("jiuce"), []byte("juice"))
+	// sl.Put([]byte("juicy"), []byte("juice"))
+	// sl.Put([]byte("change"), []byte("juice"))
+	// sl.Put([]byte("bang"), []byte("juice"))
+	// sl.Put([]byte("alpha"), []byte("juice"))
+	// sl.Put([]byte("jiuce"), []byte("juice"))
+	// sl.Put([]byte("crazy"), []byte("juice"))
+	// sl.Put([]byte("casdfasdfrazy"), []byte("juice"))
+	// sl.Put([]byte("asdf"), []byte("juice"))
 
-	sl.Print()
+	// sl.Print()
+
+	d := db.NewKVStore("hello")
+	v, err := d.Get([]byte("snail"))
+	fmt.Println(err)
+	fmt.Println(string(v))
 }
